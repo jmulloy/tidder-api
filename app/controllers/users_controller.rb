5d2acf7b61b59f6db9490ff04b8a38f84a12d140
github username <github email address>
2019-01-10 12:57:50 -0500

    initialized rails app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        respond_to do |f|
+            f.html {render :index}
+            f.json {render json: @recipes}
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to landing_path
+        else
+        
+            render :new
+        end
+    end
+
+    
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+end
