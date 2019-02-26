3bbcfc29d3d7f18ba7d2eced273fe76e68fd6bc1
github username <github email address>
2019-02-26 12:44:08 -0500

    Updated CORS.rb for deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'http://tidder-client.herokuapp.com', 'https://tidder-client.herokuapp.com'
-
+    origins %w[
+      localhost:3000
+      http://tidder-client.herokuapp.com
+      https://tidder-client.herokuapp.com
+    ]
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
