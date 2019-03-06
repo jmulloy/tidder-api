e6ad591dfbb87b7faaa575f17459f2100772e8a9
github username <github email address>
2019-03-06 09:18:48 -0500

    Add likes to strong params in post controller and add update method

--- app/controllers/API/posts_controller.rb
+++ app/controllers/API/posts_controller.rb
@@ -1,7 +1,7 @@
 module Api
 
     class PostsController < ApplicationController
-        before_action :set_post, only: [:show]
+        before_action :set_post, only: [:show, :update]
 
         def index 
             render json: Post.all
@@ -20,11 +20,18 @@ module Api
             end
         end
 
+        def update 
+            if @post.update(post_params)
+                render json: post
+            else
+                render json: {message: post.errors }, status: 400
+        end
+
 
         private
 
         def post_params
-            params.require(:post).permit(:title, :content, :author)
+            params.require(:post).permit(:title, :content, :author, :likes)
         end
 
         def set_post 
